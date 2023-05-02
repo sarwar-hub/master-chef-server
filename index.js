5c0128a237bcee07bb3d3dbb552c35d65e639b23
Sarwar <2plus7sarwar@gmail.com>
2023-05-02 12:08:40 +0600

    added functionality of getingt recipes of  specific chef

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const chef = require('./data/chef.json');
-const recipe = require('./data/recipe.json');
+
+
+const chefs = require('./data/chef.json');
+const recipes = require('./data/recipe.json');
+
 
 app.get('/', (req, res) => {
     res.send('master-chef API server');
 })
 
+
+// get chefs
 app.get('/chef', (req, res) => {
-    res.send(chef);
+    res.send(chefs);
 })
+
+// get recipes
 app.get('/recipe', (req, res) => {
-    res.send(recipe);
+    res.send(recipes);
+})
+
+
+// find all recipes of specific chef
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const recipeByChef = recipes.filter(recipe => recipe.chef_id === id);
+    res.send(recipeByChef || []);
 })
 
 app.listen(port, () => {
